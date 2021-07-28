abced1cb5537c0035e84d87789c7723680535f01
Ajay KC <meajay64@gmail.com>
2021-07-28 23:20:28 +0900

    Refactor

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const form = document.getElementById('user-form');
 let userInput = document.querySelector('#user-input');
-let divContainer = document.querySelector('.to-do');
+let toDoItemsContainer = document.querySelector('.to-do');
 
-console.log(divContainer);
-form.addEventListener('submit',onSubmit);
-
-
+console.log(toDoItemsContainer);
+form.addEventListener('submit',toDoLogic);
 
 
 
-function onSubmit(event) {
+//Function Declarations...
+function toDoLogic(event) {
     addToDoElement();
     form.reset();
     event.preventDefault();
@@ -25,8 +24,8 @@ function addToDoElement() {
     button.textContent = 'Delete';
         
     div.classList.add('flex');
-    
+
     div.appendChild(para);
     div.appendChild(button);
-    divContainer.appendChild(div);
+    toDoItemsContainer.appendChild(div);
 }
