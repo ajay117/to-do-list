e5fe494e4a87e97d959699f7e5fad038929d58ab
Ajay KC <meajay64@gmail.com>
2021-07-30 23:51:32 +0900

    Added functionalities such as for checkbox and form pop up on user need

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const form = document.getElementById('user-form');
 let userInput = document.querySelector('#user-input');
-let toDoItemsContainer = document.querySelector('.to-do');
+let toDoItemsContainer = document.querySelector('.to-do-list');
+let addToDoMarker = document.querySelector('.add-to-do-marker');
+let submitButton = document.querySelector('.submit-button');
 
-console.log(toDoItemsContainer);
+
 form.addEventListener('submit',toDoLogic);
-
-
+addToDoMarker.addEventListener('click', () => {
+    popUpForm();
+    userInput.focus();
+});
+submitButton.addEventListener('click',hideForm);
 
 //Function Declarations...
 function toDoLogic(event) {
@@ -19,13 +24,46 @@ function addToDoElement() {
     let div = document.createElement('div');
     let para = document.createElement('p');
     let button = document.createElement('button');
-
+    let createCheckbox = document.createElement('input');
+    
+    createCheckbox.type = 'checkbox';
     para.textContent = userToDo;
-    button.textContent = 'Delete';
+    button.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    button.style.width = '25px';
+    button.style.borderRadius = '5px';
+    button.classList.add('btn');
+    button.classList.add('btn-danger');
         
-    div.classList.add('flex');
+    div.classList.add('flex','flex-align-baseline');
+    div.style.margin = '0 0 10px 0';
 
+    createCheckbox.classList.add('flex-child');
+    para.classList.add('flex-child');
+    button.classList.add('flex-child');
+    
+
+    div.appendChild(createCheckbox);
     div.appendChild(para);
     div.appendChild(button);
     toDoItemsContainer.appendChild(div);
+    
+    
+    button.addEventListener('click', () => {
+        toDoItemsContainer.removeChild(div);
+    });
+    createCheckbox.addEventListener('change', (e) => {
+        if(e.target.checked) {
+            para.classList.add('task-complete');
+        } else {
+            para.classList.remove('task-complete');
+        }
+    });
+}
+
+function popUpForm() {
+    form.classList.remove('hide');
+}
+
+function hideForm() {
+    form.classList.add('hide');
 }
