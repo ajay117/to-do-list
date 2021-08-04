87025294972eb3f027966ea97c6366ed4e8096db
Ajay KC <meajay64@gmail.com>
2021-08-04 20:28:25 +0900

    Now user cannot leave inputs

--- src/modules/toDoLogic.js
+++ src/modules/toDoLogic.js
@@ -3,10 +3,16 @@ import { toDoContainerPopulate } from "./toDoContainerPopulate.js";
 import { fillDefaultValues } from "./fillDefaultValues.js";
 import { toDoItemsContainer, toDoObjectArray, form } from "../script.js";
 import { toDoTitlePopulate } from "./toDoTitlePopulate.js";
-
+import { userInputTitle, userInputDescription } from '../script.js';
 
 export default function toDoLogic(e) {
     e.preventDefault();
+    if(!(userInputTitle.value && userInputDescription.value)) {
+        toDoObjectArray.forEach(obj => toDoContainerPopulate(obj));
+        console.log('ok');
+        fillDefaultValues();
+        return;        
+    }
     toDoObjectPopulate();
     toDoContainerPopulate(toDoObjectArray[toDoObjectArray.length -1]);
     toDoItemsContainer.classList.remove('hide');
